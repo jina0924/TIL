1eb272993afcfd791460da6505bc1c96718b4b75
jina jeong <jina092400@gmail.com>
2022-03-20 23:08:26 +0900

    Study django model

--- 04_01_django_practice/01_django_model/articles/urls.py
+++ 04_01_django_practice/01_django_model/articles/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from . import views
+
+app_name = 'articles'
 
 urlpatterns = [
-    
+    # articles/
+    path('', views.index, name='index'),
+
+    # articles/new/ -> throw
+    path('new/', views.new, name='new'),
+
+    path('create/', views.create, name='create'),
 ]
--- 04_01_django_practice/01_django_model/articles/views.py
+++ 04_01_django_practice/01_django_model/articles/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    # 사용자가 게시글을 입력할 수 있는 폼을 제공
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article = Article(title=title, content=content)
+    article.save()
+    return redirect('articles:index')
