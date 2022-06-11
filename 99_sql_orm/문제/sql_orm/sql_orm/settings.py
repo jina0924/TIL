9757e2b71aaa7a33834c47223e43adfed7a7176c
jina jeong <jina092400@gmail.com>
2022-06-11 17:25:23 +0900

    Modify django settings

--- 99_sql_orm/문제/sql_orm/sql_orm/settings.py
+++ 99_sql_orm/문제/sql_orm/sql_orm/settings.py
@@ -19,8 +19,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+ct$z8i8%mu3u^yn3r9m6gv=0#d!c#1l4*qgpcuf%x7gz4m27t'
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file, 'r') as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
