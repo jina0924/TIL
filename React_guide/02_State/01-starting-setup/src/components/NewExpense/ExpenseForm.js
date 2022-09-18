b0d7ad3a59f96bc13469bd36f98c012dbcf7ed28
jina jeong <jina092400@gmail.com>
2022-09-18 21:16:08 +0900

    Study react State

--- React_guide/02_State/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React_guide/02_State/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -6,17 +6,40 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitlte: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // }); // 리액트가 상태 업데이트 스케쥴을 갖고 있어서 바로 실행 x => 동시에 수많은 상태 업데이트를 계획한다면 이 접근이 적절함
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
