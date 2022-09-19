5f502116813c1f3b60293edc2975d40f5da4efaf
jina0924 <jinajung73@naver.com>
2022-09-19 15:05:42 +0900

    Stduy react rendering list

---
+++ React_guide/03_RenderingList_ContionalContent/01-starting-setup/src/components/Expenses/ExpenseList.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./ExpenseList.css";
+
+function ExpenseList(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    // console.log("Expenses.js");
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
+
+export default ExpenseList;
---
+++ React_guide/03_RenderingList_ContionalContent/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import "./NewExpense.css";
+
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(), // 이론적으로는 id를 대체할 수 있는 값은 아님(이 프로젝트에서는 상관x)
+    };
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
